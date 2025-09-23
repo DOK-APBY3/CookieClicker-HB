7dbc37ed2b286242ebd30b74e6f3306f0a3d77c4
DOK-APBY3 <arbuz2006@bk.ru>
2025-09-23 11:29:39 +0700

    added Enemy class, and constructor

---
+++ CookieClicker-HB/EnemyTemplate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookieClicker_HB
+{
+    class EnemyTemplate
+    {
+        private string _name, _iconName;
+        private int _baseLife, _baseGold;
+        private double _lifeModifier, _goldModifier, _spawnRate;
+
+        public EnemyTemplate(string name, string iconName, int baseLife, int baseGold, double lifeModifier, double goldModifier, double spawnRate)
+        {
+            _name = name;
+            _iconName = iconName;
+            _baseLife = baseLife;
+            _baseGold = baseGold;
+            _lifeModifier = lifeModifier;
+            _goldModifier = goldModifier;
+            _spawnRate = spawnRate;
+        }
+
+        
+
+
+
+    }
+}
