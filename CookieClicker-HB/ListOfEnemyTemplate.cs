f72d7df659cb97545614b80ae5bc0266860d53d7
DOK-APBY3 <arbuz2006@bk.ru>
2025-09-23 20:06:59 +0700

    added al lot of functions to List Of enemys

--- CookieClicker-HB/ListOfEnemyTemplate.cs
+++ CookieClicker-HB/ListOfEnemyTemplate.cs
@@ -8,7 +8,30 @@ namespace CookieClicker_HB
 {
     class ListOfEnemyTemplate
     {
-        
+        List<EnemyTemplate> enemies;
+
+        public ListOfEnemyTemplate()
+        {
+            enemies = new List<EnemyTemplate>();
+        }
+
+        public void addEnemy(string name, string iconName, int baseLife, int baseGold, double lifeModifier, double goldModifier, double spawnRate)
+        {
+            EnemyTemplate tmpEnemy = new EnemyTemplate(name, iconName, baseLife, baseGold, lifeModifier, goldModifier, spawnRate);
+            enemies.Add(tmpEnemy);
+        }
+
+        public EnemyTemplate getEnemyByName(string name)
+        {
+            EnemyTemplate tmpEnemy = enemies.FirstOrDefault(enemy => enemy.Name == name);
+            return tmpEnemy;
+
+            // надо будет на строне приёма сделать обработчиr для случаев когда нет такого имени
+        }
+        public EnemyTemplate getEnemyByIndex(int index)
+        {
+            return enemies[index];
+        }
 
 
     }
