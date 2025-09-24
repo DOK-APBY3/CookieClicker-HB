b4f299685bf32b7e7f4cb92a0067ea5661142a11
DOK-APBY3 <arbuz2006@bk.ru>
2025-09-24 09:15:34 +0700

    added some saving and loading utilities

---
+++ CookieClicker-HB/FileManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows;
+
+namespace CookieClicker_HB
+{
+    class FileManager
+    {
+
+        private readonly string lastFilePathFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "WpfBUZMAZ_lastFile.txt");
+        private string lastFilePath;
+
+
+        private void LastSavedFinder()
+        {
+
+            if (File.Exists(lastFilePathFile))
+            {
+                lastFilePath = File.ReadAllText(lastFilePathFile);
+            }
+        }
+        private void LastSavedLoader()
+        {
+
+            if (File.Exists(lastFilePathFile))
+            {
+                string lastFilePath = File.ReadAllText(lastFilePathFile);
+                if (File.Exists(lastFilePath))
+                {
+                    ListOfEnemyTemplate loader = new ListOfEnemyTemplate();
+                    loader.loadFromJson(lastFilePath);
+                }
+            }
+        }
+
+        private void LoadFromFile(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            
+            dlg.FileName = "Document";
+            dlg.DefaultExt = ".json";
+            dlg.Filter = "Text documents (.json)|*.json";
+            dlg.ShowDialog();
+            string lb1 = dlg.FileName;
+
+            ListOfEnemyTemplate loader = new ListOfEnemyTemplate();
+            loader.loadFromJson(lb1);
+        }
+
+
+    }
+}
