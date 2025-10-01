78a2598a61fe13139229dd30211c4686e54e6a33
DOK-APBY3 <arbuz2006@bk.ru>
2025-10-01 17:07:00 +0700

    prepared to adding interface and buttons

--- CookieClicker-HB/MainWindow.xaml.cs
+++ CookieClicker-HB/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,23 +20,61 @@ public partial class MainWindow : Window
 {
     Random rnd = new Random();
 
+    ListOfEnemyTemplate enemyList = new ListOfEnemyTemplate();
+
     public MainWindow()
     {
         InitializeComponent();
 
-        ListOfEnemyTemplate enemyList1 = new ListOfEnemyTemplate();
-
-        enemy_AddRandomByNameToList("labubsli", "stalnoy zad", "bosses", enemyList1);
-
-        FileManager.SaveToSelectedFile(enemyList1);
-
-        //listOfEnemys.deleteEnemyByIndex(42);
-
-
-        
-
-        FileManager.LoadFromSelectedFile(enemyList1);
+        testing();
     }
+
+    private void testing()
+    {
+        enemy_AddRandomByNameToList("labubsli", "stalnoy zad", "bosses", enemyList);
+        //listOfEnemys.deleteEnemyByIndex(42);
+    }
+
+    public void deleteEnemyWithThisName(string name)
+    {
+        enemyList.deleteEnemyByName(name);
+    }
+
+    public void deleteEnemyWithThisIndex(int index)
+    {
+        enemyList.deleteEnemyByIndex(index);
+    }
+
+    public void takeEnemyWithThisName(string name)
+    {
+        enemyList.getEnemyByName(name);
+    }
+
+    public void takeEnemyWithThisIndex(int index)
+    {
+        enemyList.getEnemyByIndex(index);
+    }
+
+    //public void AddCurrentEnemy()  будет работать когда будеи интерфейс
+    //{
+    //    enemyList.addEnemy(name, iconName, groupe,
+    //        rnd.Next(1, 10),
+    //        rnd.Next(1, 10),
+    //        Math.Round(rnd.NextDouble() * 10, 2),
+    //        Math.Round(rnd.NextDouble() * 10, 2),
+    //        Math.Round(rnd.NextDouble(), 2));
+    //}
+
+    public void saveListOfEnemies()
+    {
+        FileManager.SaveToSelectedFile(enemyList);
+    }
+
+    public void loadListOfEnemies()
+    {
+        FileManager.LoadFromSelectedFile(enemyList);
+    }
+
 
     public void enemy_AddRandomByNameToList(string name, string iconName, string groupe, ListOfEnemyTemplate neededList)
     {
